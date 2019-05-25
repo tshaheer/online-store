c2140d99322a714aeb76cb75bbed7e0349e3cc5f
Shaheer Thooppil <shaheer.aitrich@gmail.com>
2019-05-25 08:05:40 +0530

    Progress bar for navigation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,11 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { NgProgressModule } from '@ngx-progressbar/core'
+import { NgProgressRouterModule } from '@ngx-progressbar/router'
+import { NgProgressHttpModule } from '@ngx-progressbar/http'
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
@@ -8,8 +13,8 @@ import { EnsureModuleLoadedOnceGuard } from './ensure-module-loaded-once-guard';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, RouterModule],
-  exports: [ NavbarComponent, FooterComponent, RouterModule],
+  imports: [CommonModule, SharedModule, RouterModule, HttpClientModule, NgProgressModule, NgProgressRouterModule, NgProgressHttpModule],
+  exports: [NavbarComponent, FooterComponent, RouterModule, HttpClientModule, NgProgressModule, NgProgressRouterModule, NgProgressHttpModule],
   declarations: [NavbarComponent, FooterComponent]
 })
 export class CoreModule extends EnsureModuleLoadedOnceGuard  { // Ensure that CoreModule is only loaded into AppModule
